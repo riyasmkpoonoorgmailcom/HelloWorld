1fd873283c7771f3d7bfbe7873d24adeb5dc89b2
Riyas M K <riyasmkpoonoor@gmail.com>
2020-05-04 13:16:56 +0530

    first change in code

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i = 1;i<=10;i++)
+		for(int i = 1;i<=20;i++)
 		{
 			System.out.println("Hello world..."+i);
 		}
